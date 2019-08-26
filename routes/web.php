05d0ddad7b849bc68523ccb3495ac9a61fea3b06
tunchamroeun <chamroeuntun25@outlook.com>
2019-08-26 11:25:46 +0700

    update login

--- routes/web.php
+++ routes/web.php
@@ -12,25 +12,27 @@
 */
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('patient.index');
-});
-Route::get('/media', function () {
-    return view('media');
-})->name('media');
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('/user', 'UserController');
-Route::post('/user-list', 'UserController@list')->name('user.list');
-/*patient*/
-Route::resource('/patient','PatientController');
-Route::post('/patient-list','PatientController@list')->name('patient.list');
-/*echo*/
-Route::resource('/echo','EchoController');
-Route::get('/echo-print-page/{id}','EchoController@_print')->name('echo.print');
-Route::resource('/echo-template','EchoTemplateController');
-Route::post('/echo-template-search','EchoTemplateController@search')->name('echo.template.search');
-/*labor*/
-Route::resource('/labor','LaboController');
-Route::get('/labor-print-page/{id}','LaboController@_print')->name('labor.print');
+Route::middleware(['web', 'auth'])->group(function () {
+    Route::get('/', function () {
+        return view('patient.index');
+    });
+    Route::get('/media', function () {
+        return view('media');
+    })->name('media');
+
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('/user', 'UserController');
+    Route::post('/user-list', 'UserController@list')->name('user.list');
+    /*patient*/
+    Route::resource('/patient', 'PatientController');
+    Route::post('/patient-list', 'PatientController@list')->name('patient.list');
+    /*echo*/
+    Route::resource('/echo', 'EchoController');
+    Route::get('/echo-print-page/{id}', 'EchoController@_print')->name('echo.print');
+    Route::resource('/echo-template', 'EchoTemplateController');
+    Route::post('/echo-template-search', 'EchoTemplateController@search')->name('echo.template.search');
+    /*labor*/
+    Route::resource('/labor', 'LaboController');
+    Route::get('/labor-print-page/{id}', 'LaboController@_print')->name('labor.print');
+});
